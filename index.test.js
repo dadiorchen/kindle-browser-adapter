689b27af45f4fbd5e965b4b22a7d9ac635d4d1c7
dadiorchen <dadiorchen@outlook.com>
2021-12-14 06:47:43 +0800

    feat: be able to read oop with image

--- index.test.js
+++ index.test.js
@@ -54,7 +54,13 @@ axios({
     }),
 );
 
-it.skip("read oop html, get reader mode content, output to a file", async () => {
+function extractName(url) {
+  const m = url.match(/^https?:\/\/.*\/([^/]*(png|svg|jpeg))/i);
+  const imageName = m[1];
+  return imageName;
+}
+
+it.only("read oop html, get reader mode content, output to a file", async () => {
   const fs = require("fs");
   var { Readability } = require('@mozilla/readability');
   var { JSDOM } = require('jsdom');
@@ -77,11 +83,13 @@ it.skip("read oop html, get reader mode content, output to a file", async () => 
   for(let img of imgs) {
     const url0 = img.match(/src="([^"]*?)"/)[1];
     expect(url0).toMatch(/^https?:\/\//);
+    const imageName = extractName(url0);
     log.warn("image url:", url0);
+    const path = `./out/${imageName}`;
+    await download_image(url0, path);
     const index = content.search(url0);
     expect(index).toBeGreaterThan(1);
-    content = content.replace(url0, 'xxx');
-    expect(content).toMatch(/xxx/);
+    content = content.replace(url0, `./${imageName}`);
   }
 
   const template = fs.readFileSync("./template.html", "utf8");
@@ -94,10 +102,9 @@ it.skip("read oop html, get reader mode content, output to a file", async () => 
 
 });
 
-it.only("down load image, and save to file: https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Octicons-terminal.svg/24px-Octicons-terminal.svg.png", async () => {
+it("down load image, and save to file: https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Octicons-terminal.svg/24px-Octicons-terminal.svg.png", async () => {
   const url = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Octicons-terminal.svg/24px-Octicons-terminal.svg.png";
-  const m = url.match(/^https?:\/\/.*\/([^/]*(png|svg|jpeg))/i);
-  const imageName = m[1];
+  const imageName = extractName(url);
   expect(imageName).toBe("24px-Octicons-terminal.svg.png");
   await download_image(url, `out/${imageName}`);
   // file exists
